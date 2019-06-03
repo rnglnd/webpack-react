165c172346fca5fea0c946ddd3fbf083476fd7b5
Steven Ringland <steven.ringland@bazaarvoice.com>
2019-06-03 19:27:56 +0200

    init 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
-import "../css/main.css";
+import React from 'react';
+import { render } from 'react-dom';
+import '../css/main.css';
 
-const Index = () => <div className="header">React</div>;
+const Index = () => (
+  <div>hello</div>
+);
 
 export default Index;
 
-const div = document.createElement("div");
+const div = document.createElement('div');
 document.body.appendChild(div);
 
 render(<Index />, div);
